bdf6171fb1300380377a7b38ae6c5bba61b35e8a
Carrotw <hernandez50560401@hotmail.es>
2023-11-07 01:19:13 -0500

    Finish: Método de la secante

--- src/components/Modal/SecanteModificada.jsx
+++ src/components/Modal/SecanteModificada.jsx
@@ -1,42 +1,21 @@
-import {Col} from "react-bootstrap";
+import {Col, Container} from "react-bootstrap";
 import { InlineMath } from 'react-katex';
 import functionPlot from "function-plot";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 
 export default function SecanteModificada(){
 
-  const expresionSecanteModificada = 'f(h_2) = \\sqrt{2 * g * (h_2 - h_1) }-100m/s';
-
-  function calculateSecante(){
-    function f(x){
-      const g = 9.81
-      const h_1 = 20
-      return (Math.sqrt(2*g*(x-h_1))-100)
-    }
-    const d = 0.01
-    let x_0 = 20
-    let iteracion = 0
-    const resultados = []
-
-    while(f(x_0) !== 0){
-      iteracion++
-      let x_1 = x_0 - (f(x_0)*(d*x_0))/(f(x_0+(d*x_0))-f(x_0))
-      console.log("Iteración: " + iteracion,x_1, f(x_1))
-      resultados.push({
-        x0: x_0,
-        x1: x_1
-      })
-      x_0 = x_1
-      // Añadir a la lista
-    }
-
-    console.log(resultados)
-    return resultados
-  }
+  const [raiz, setRaiz] = useState(NaN);
+  const [resultadosProcedimientos, setResultadosProcedimientos] = useState([])
+  const d = 0.01
+  const h_1 = 20
+  const g = 9.81
+  const expresionSecanteModificada = `f(h_2) = \\sqrt{2 * g * (h_2 - ${h_1}) }-100m/s = ${raiz} `;
+  const resultados = []
 
   useEffect(() => {
-    const resultados = calculateSecante()
     functionPlot({
+      title: "Gráfica de la secante modificada",
       target: '#graficasecante',
       yAxis: {domain: [-2, 2]},
       xAxis: {domain: [(529 - 20), (529 + 20)]},
@@ -45,7 +24,71 @@ export default function SecanteModificada(){
         secants: resultados
       }]
     })
+    recalculate();
   }, []);
+
+
+
+  function f(x){
+    return (Math.sqrt(2*g*(x-h_1))-100)
+  }
+
+  function recalculate(){
+    setResultadosProcedimientos([])
+    resultados.length = 0
+    let iteracion = 0
+    const d = 0.01
+    let x_0 = h_1
+    let error = 100
+
+    while(f(x_0) !== 0 && error > 0.01){
+      iteracion+=1
+      let x_1 = x_0 - (f(x_0)*(d*x_0))/(f(x_0+(d*x_0))-f(x_0))
+      error = Math.abs((x_0-x_1)/x_0)*100
+      const temp = estilizar(iteracion, x_0, x_1, error)
+      setResultadosProcedimientos(resultadosProcedimientos => [...resultadosProcedimientos, temp])
+      resultados.push({
+        x0: x_0,
+        x1: x_1
+      })
+
+      x_0 = x_1
+    }
+
+    setRaiz(x_0)
+  }
+
+  function estilizar(iteracion, x0, x1, error){
+
+    console.log("Iteración: ", iteracion, "x0: ", x0, "x1: ", x1, "error: ", error)
+
+    const decimales = 4
+    x0 = parseFloat(x0.toFixed(decimales))
+    x1 = parseFloat(x1.toFixed(decimales))
+
+    return(
+      <Container
+        className='p-2'
+      >
+        <h4>
+          Iteración {iteracion}, x<sub>0</sub> = {x0}
+        </h4>
+        <InlineMath math={`x_i = ${x0} - \\frac{ ${d} ${x0} * ${f(x0)} }{ ${f(x0+(d*x0))} - ${f(x0)}} = ${x1}`} />
+        <br/><br/>
+        <InlineMath math={`f(${x1}) = \\sqrt{2 * ${g} * (${x1} - ${h_1}) }-100m/s = ${f(x1).toFixed(4)}`} />
+        <br/><br/>
+        <InlineMath math={`E_r = |\\frac{ ${x0}-${x1}}{${x0}}| * 100\\% = ${error} \\%`}/>
+      </Container>
+    )
+  }
+
+  function getProcedimiento(){
+    return(
+      <Container>
+        {resultadosProcedimientos}
+      </Container>
+    )
+  }
 
   return(
     <Col xs={6} style={{padding: '0px',}}
@@ -76,11 +119,19 @@ export default function SecanteModificada(){
       </p>
 
       <section>
-        blabl abla balbaaejgbiusgrele
-        r fiwvbeuwtfhr-g
-        .ñ
-        egwf- e
-        g
+        <p>
+          El método de la secante establece que:
+        </p>
+        <InlineMath math={'x_i = x_0 - \\frac{dx_0*f(x_0)}{f(x_0+dx_0)-f(x_0)}'} />
+        &nbsp;&nbsp;&nbsp;
+        <InlineMath math={'E_r = |\\frac{x_0-x_i}{x_0}| * 100\\%'}/>
+        <br/> <br/>
+        <Container>
+          Para hallar el valor del nivel de agua que necesitaremos para que la velocidad de salida del agua sea de 100m/s, utilizaremos el método de la secante modificada para hallar el valor de x iterando hasta que el error sea menor a 0.01% o hasta encontrar la raíz.
+        </Container>
+
+        {getProcedimiento()}
+
       </section>
 
     </Col>
